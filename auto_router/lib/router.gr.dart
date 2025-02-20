85b7c3fae39dea568ecfd8af5c70cdae0f5e5fed
Pavithra M <144348676+pavithramoorthy-official@users.noreply.github.com>
2025-02-20 14:03:06 +0530

    auto_route package usage simple project

---
+++ auto_router/lib/router.gr.dart
@@ -0,0 +1,67 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'router.dart';
+
+/// generated route for
+/// [FirstPage]
+class FirstRoute extends PageRouteInfo<void> {
+  const FirstRoute({List<PageRouteInfo>? children})
+      : super(
+          FirstRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'FirstRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const FirstPage();
+    },
+  );
+}
+
+/// generated route for
+/// [PageOne]
+class RouteOne extends PageRouteInfo<void> {
+  const RouteOne({List<PageRouteInfo>? children})
+      : super(
+          RouteOne.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RouteOne';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const PageOne();
+    },
+  );
+}
+
+/// generated route for
+/// [PageTwo]
+class RouteTwo extends PageRouteInfo<void> {
+  const RouteTwo({List<PageRouteInfo>? children})
+      : super(
+          RouteTwo.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RouteTwo';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const PageTwo();
+    },
+  );
+}
